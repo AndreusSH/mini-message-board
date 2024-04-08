63afd4f627907187fa343cc0ee3d978a94ed8725
AndreusSH <andreamammoliti@yahoo.it>
2024-04-08 15:42:06 +0200

    Posting new messages

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const express = require('express')
 const path = require('path')
 const messages = require('./messages'); // Importing messages object
+const fs = require('fs');
+
 
 const app = express()
 
 
-app.set('views', 'C:\\Users\\andre\\OneDrive\\Desktop\\mini-message-board\\templates');
-
+app.set('views', path.join(__dirname, 'templates')); 
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
 
+
+// Middleware to parse incoming request bodies
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 8080
 app.get('/', function (req, res) {
     res.render('messages', { messages: messages });
 })
+app.get('/form', function (req, res) {
+    
+    res.render('form');
+})
 
-
+app.post('/new', function (req, res) {
+    console.log(req.body); 
+    const { user, text } = req.body;
+    const currentDate = new Date();
+    messages.push({ user, text, added: currentDate });
+    fs.writeFileSync('./messages.js', 'module.exports = ' + JSON.stringify(messages, null, 4));
+     // Redirect back to home
+    res.redirect('/');
+});
 app.listen(port)
 console.log('Server started at http://localhost:' + port)
--- messages.js
+++ messages.js
@@ -1,15 +1,17 @@
-const messages = [
+module.exports = [
     {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
+        "text": "Hi there!",
+        "user": "Amando",
+        "added": "2024-04-08T13:40:34.257Z"
     },
     {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
+        "text": "Hello World!",
+        "user": "Charles",
+        "added": "2024-04-08T13:40:34.257Z"
+    },
+    {
+        "user": "Andrea",
+        "text": "Prova",
+        "added": "2024-04-08T13:40:49.264Z"
     }
- ];
- 
-
- module.exports = messages;
+]

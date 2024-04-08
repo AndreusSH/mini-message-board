f96d1ff12be5340df76837c4cf7a62230d35ac06
AndreusSH <andreamammoliti@yahoo.it>
2024-04-08 14:56:08 +0200

    Setting ejs as template engine and display messages

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const path = require('path')
+const messages = require('./messages'); // Importing messages object
 
 const app = express()
+
+
+app.set('views', 'C:\\Users\\andre\\OneDrive\\Desktop\\mini-message-board\\templates');
+
+// Set EJS as the template engine
+app.set('view engine', 'ejs');
+
+
 const port = process.env.PORT || 8080
 app.get('/', function (req, res) {
-  res.send('Is the server working?')
+    res.render('messages', { messages: messages });
 })
 
 
